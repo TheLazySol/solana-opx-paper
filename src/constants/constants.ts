1e2e94a731e296c69e1665baccd929c66c406b45
TheLazySol <tbelonog25@gmail.com>
2025-03-20 19:19:04 -0400

    Asset Price Refresh reduced to 5 sec updates

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -45,7 +45,7 @@ export type ExpirationDate = {
 }
 
 // Asset Price Refresh Interval
-export const ASSET_PRICE_REFRESH_INTERVAL = 1000; // 1 second
+export const ASSET_PRICE_REFRESH_INTERVAL = 5000; // 5 seconds
 
 // SOL PH Volatility
 export const SOL_PH_VOLATILITY = 0.45;
