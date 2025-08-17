be4ca57b59de825499b1f7662914b23372413a7b
TheLazySol <tbelonog25@gmail.com>
2025-08-17 16:51:38 -0400

    removed unnecessary migration files

--- prisma/migrations/20250817185646_remove_ip_tracking/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ipAddress` on the `user_sessions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."user_sessions" DROP COLUMN "ipAddress";
